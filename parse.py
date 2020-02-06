bf5b85c00021b70d9ccda84f0ca9786c3c612ac4
Desrues <gaetan.desrues@free.fr>
2020-02-06 10:52:32 +0100

    ok

--- parse.py
+++ parse.py
@@ -2,52 +2,48 @@
 import urllib.request, urllib.error, urllib.parse
 from bs4 import BeautifulSoup
 import pandas as pd
+import os
 
 def main():
-    download = False
-    if download:
+    filename, forceDownload = 'webpage.html', False
+    if not os.path.exists(filename) or forceDownload:
         url = 'https://www.tripadvisor.fr/Hotels-g187497-Barcelona_Catalonia-Hotels.html'
-        # Telecharge le contenu du fichier html
-        response = urllib.request.urlopen(url)
+        response = urllib.request.urlopen(url) # Telecharge le contenu du fichier html
         webContent = response.read().decode("utf-8")
-        # On peut sauvegarde dans un fichier
-        with open('webpage.html', 'w') as f:
-            f.write(webContent)
+        with open(filename,'w') as f:
+            f.write(webContent) # On peut sauvegarde dans un fichier
     else:
-        # Pour éviter de re-télécharger:
-        with open('webpage.html', 'r') as f:
-            webContent = f.read()
+        with open(filename,'r') as f:
+            webContent = f.read() # Pour éviter de re-télécharger: on lit le fichier
 
+    # ** webContent contient le contenu html **
 
     # On parse le html
     soup = BeautifulSoup(webContent, 'html.parser')
 
-    # sous forme de liste d'éléments: liste de tous les hôtels (blocs principaux de la page web)
+    # boxes contient la liste de tous les hôtels (liste des blocs principaux de la page web)
     boxes = soup.find_all("div", {"class": "listItem"})
     # print(f"J'ai trouvé {len(boxes)} conteneurs d'un élément de liste")
 
     infos = list() # liste de listes
-    # Pour chaque hotel de la liste des hôtels
-    for box in boxes:
-        # Dans le container, cherche les avis
+    for box in boxes: # Pour chaque hotel de la liste des hôtels
+        # Dans le container, cherche toutes les infos
         nbAvis, prix, nom = extractData(box)
 
+        # Et on les ajoute a la liste globale
         if prix is not None:
             infos_box = [nom, nbAvis, prix]
             infos.append(infos_box)
 
+
     # infos a toutes les infos, on trie et affiche:
-    postProcessing(infos)
-
-
-
-def postProcessing(infos):
     db = pd.DataFrame(infos, columns=['Nom','Avis','Prix'])
     # Critères
-    print("** Filtre: la liste des hotels a moins de 70e la nuit et plus de 500 avis **")
+    print("\n** Filtre: la liste des hotels a moins de 70e la nuit et plus de 500 avis **")
     filtered = db.loc[(db['Prix']<70) & (db['Avis']>500),:]
     for index, row in filtered.iterrows():
         print(f"-> {row['Nom']}: {row['Prix']}e, {row['Avis']} avis")
+    print()
 
 
 
@@ -67,8 +63,8 @@ def extractData(box):
         prix = int(str_price)
     # print(f"La nuit coûte {prix} euros")
 
-    # Et le nom (par le container parent)
-    title = box.find('div', {'class': 'listing_title'}).findChild("a" , recursive=False)
+    # Et le nom
+    title = box.find('a', {'class': 'property_title'})
     nomHotel = title.string
 
     return nbAvis, prix, nomHotel
